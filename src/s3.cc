8c4ba8d6f583a0363b66ec520d8f38014225dfb6
Jason Short <jason.short@wpengine.com>
2018-03-19 11:50:30 -0500

    build with OpenSSL 1.1.0

--- src/s3.cc
+++ src/s3.cc
@@ -822,7 +822,13 @@ void HttpMethod::SendReq(FetchItem *Itm,CircleBuf &Out)
 }
 
 void doEncrypt(char *kString, char *sigString, const char* secretKey){
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+	// OpenSSL < 1.1.0
 	HMAC_CTX hctx;
+    #else
+	// OpenSSL >= 1.1.0
+	HMAC_CTX *hctx = HMAC_CTX_new();
+    #endif
 	BIO *bio, *b64;
 	char *sigptr;
 	long siglen = -1;
@@ -832,8 +838,15 @@ void doEncrypt(char *kString, char *sigString, const char* secretKey){
 
 	// Initialize SHA1 encryption
 	sprintf(skey, "%s", secretKey);
+
+	#if OPENSSL_VERSION_NUMBER < 0x10100000L
+	// OpenSSL < 1.1.0
 	HMAC_CTX_init(&hctx);
 	HMAC_Init(&hctx, skey, (int)strlen((char *)skey), EVP_sha1());
+	#else
+	// OpenSSL >= 1.1.0
+	HMAC_Init_ex(hctx, skey, (int)strlen((char *)skey), EVP_sha1(), NULL);
+	#endif
 
 	// Encrypt
 	HMAC(EVP_sha1(), skey, (int)strlen((char *)skey), (unsigned char *)kString,
@@ -853,7 +866,14 @@ void doEncrypt(char *kString, char *sigString, const char* secretKey){
 
 	// Clean up Encryption, Encoding
 	BIO_free_all(bio);
+
+	#if OPENSSL_VERSION_NUMBER < 0x10100000L
+	// OpenSSL < 1.1.0
 	HMAC_CTX_cleanup(&hctx);
+	#else
+	// OpenSSL >= 1.1.0
+	HMAC_CTX_free(hctx);
+	#endif
 }
 									/*}}}*/
 // HttpMethod::Go - Run a single loop					/*{{{*/
